305566249137103467a59badb93af74f34238010
gobbcuh <jajalorino@gmail.com>
2025-05-24 20:07:58 +0800

    switched to javax

--- prototype/prototype/src/main/java/controller/RegisterServlet.java
+++ prototype/prototype/src/main/java/controller/RegisterServlet.java
@@ -1,7 +1,7 @@
 package controller;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
